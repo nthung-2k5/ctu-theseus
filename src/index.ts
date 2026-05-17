956c5ad60a696109eb39c10b2d210b6d8b72d951
nthung-2k5 <hungthitkhia@gmail.com>
2026-05-17 12:00:34 +0700

    scaffold project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { Elysia } from "elysia";
+import { staticPlugin } from '@elysia/static'
+import { Elysia } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(
+    await staticPlugin({
+      prefix: '/',
+      bunFullstack: true
+    }),
+  )
+  .listen(3000)
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
