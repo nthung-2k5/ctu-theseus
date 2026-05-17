27c8648ed194f05d34732de42d1b665738afd1a3
nthung-2k5 <hungthitkhia@gmail.com>
2026-05-17 15:55:33 +0700

    prepare some things

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { staticPlugin } from '@elysia/static'
+import index from '@public/index.html'
 import { Elysia } from 'elysia'
 
 const app = new Elysia()
   .use(
     await staticPlugin({
       prefix: '/',
-      bunFullstack: true
+      bunFullstack: true,
+      alwaysStatic: true,
     }),
   )
+  .get('/*', index)
   .listen(3000)
 
 console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
